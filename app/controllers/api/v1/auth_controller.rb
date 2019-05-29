30269815afa12f68f8adeb2b3e00dc2114f7d47a
danielmartin0126 <danielmartin0126@gmail.com>
2019-05-29 14:25:31 -0400

    auth even more done BE

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -4,25 +4,26 @@ class Api::V1::AuthController < ApplicationController
         user = User.find_by(name: params[:username])
     
         if user && user.authenticate(params[:password])
-          token = issue_token({id: user.id})
-          render json: {id: user.id, name: user.name, token: token}
+
+        #   token = issue_token({id: user.id})
+          render json: {id: user.id, name: user.name}
         else
           render json: {error: "Could not authenticate"}, status: 401
         end
       end
     
-      def show
-        # token = request.headers['Authenticate']
-        # decoded = JWT.decode(token, 'secret', true, { algorithm: 'HS256' }).first
-        # id = decoded["id"]
-        #
-        # user = User.find_by(id: id)
+    #   def show
+    #     # token = request.headers['Authenticate']
+    #     # decoded = JWT.decode(token, 'secret', true, { algorithm: 'HS256' }).first
+    #     # id = decoded["id"]
+    #     #
+    #     # user = User.find_by(id: id)
     
-        if current_user
-          render json: {id: current_user.id, username: current_user.username}
-        else
-          render json: {error: "Could not authenticate"}, status: 401
-        end
-      end
+    #     if current_user
+    #       render json: {id: current_user.id, username: current_user.username}
+    #     else
+    #       render json: {error: "Could not authenticate"}, status: 401
+    #     end
+    #   end
       
 end
