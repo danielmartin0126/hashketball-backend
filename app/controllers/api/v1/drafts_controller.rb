cd55a7b1af58948db220d33cfec6ef056a48edcb
joshuamsmith413 <joshuamsmith413@gmail.com>
2019-05-30 11:08:46 -0400

    delete feature

--- app/controllers/api/v1/drafts_controller.rb
+++ app/controllers/api/v1/drafts_controller.rb
@@ -1,11 +1,22 @@
 class Api::V1::DraftsController < ApplicationController
 
   def create
-    draft = Draft.find_or_create_by(user_id: params[:user_id], player_id: params[:player_id])
+    @draft = Draft.find_or_create_by(user_id: params[:user_id], player_id: params[:player_id])
+    render json: @draft
   end
 
   def index
     @all = Draft.all
     render json: @all
+  end
+
+  def show
+    @draft = Draft.find(params[:id])
+    render json: @draft
+  end
+
+  def destroy
+    @draft = Draft.find(params[:id])
+    @draft.destroy
   end
 end
